6f66a747de1c0c9f91ebef20d3bdb9d983e35f75
박진솔 <jjeansp9@gmail.com>
2023-03-31 16:21:46 +0900

    commit

--- app/src/main/java/kr/co/musicplayer/activities/MainActivity.java
+++ app/src/main/java/kr/co/musicplayer/activities/MainActivity.java
@@ -5,12 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import kr.co.musicplayer.R;
+import kr.co.musicplayer.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding= ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
     }
 }
