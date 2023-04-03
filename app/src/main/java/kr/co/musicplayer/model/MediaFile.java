a4a30af8065b3b6ef09f63220298c12f3c9cea12
박진솔 <jjeansp9@gmail.com>
2023-04-03 15:30:31 +0900

    commit

---
+++ app/src/main/java/kr/co/musicplayer/model/MediaFile.java
@@ -0,0 +1,72 @@
+package kr.co.musicplayer.model;
+
+import android.net.Uri;
+
+public class MediaFile {
+
+    Uri contentUri;
+    String musicName;
+    String mimeType;
+    String relativePath;
+    String fullPath;
+    String date;
+    String size;
+
+    public Uri getContentUri() {
+        return contentUri;
+    }
+
+    public void setContentUri(Uri contentUri) {
+        this.contentUri = contentUri;
+    }
+
+    public String getMusicName() {
+        return musicName;
+    }
+
+    public void setMusicName(String musicName) {
+        this.musicName = musicName;
+    }
+
+    public String getMimeType() {
+        return mimeType;
+    }
+
+    public void setMimeType(String mimeType) {
+        this.mimeType = mimeType;
+    }
+
+    public String getRelativePath() {
+        return relativePath;
+    }
+
+    public void setRelativePath(String relativePath) {
+        this.relativePath = relativePath;
+    }
+
+    public String getFullPath() {
+        return fullPath;
+    }
+
+    public void setFullPath(String fullPath) {
+        this.fullPath = fullPath;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+}
+
+
