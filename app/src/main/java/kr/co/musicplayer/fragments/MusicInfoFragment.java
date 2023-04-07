5e7efe94a6a6fdd4027ee37998bdcc6523f2ee54
박진솔 <jjeansp9@gmail.com>
2023-04-07 18:05:39 +0900

    commit

--- app/src/main/java/kr/co/musicplayer/fragments/MusicInfoFragment.java
+++ app/src/main/java/kr/co/musicplayer/fragments/MusicInfoFragment.java
@@ -1,30 +1,17 @@
 package kr.co.musicplayer.fragments;
 
-import android.content.BroadcastReceiver;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.IBinder;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ProgressBar;
-import android.widget.SeekBar;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import kr.co.musicplayer.MusicService;
-import kr.co.musicplayer.R;
-import kr.co.musicplayer.activities.MainActivity;
 import kr.co.musicplayer.databinding.FragmentMusicInfoBinding;
-import kr.co.musicplayer.model.MediaFile;
 
 public class MusicInfoFragment extends Fragment {
 
